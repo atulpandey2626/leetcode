c993118cd8a43a6fefa9b9c06b8796edbe130748
atulpandey2626 <atulpandey262626@gmail.com>
2026-01-10 10:47:58 +0530

    Time: 0 ms (100%), Space: 17.5 MB (51.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,64 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans(2, -1);
-        int n = nums.size();
-        if (n == 0) return ans;
-
-        int low = 0, high = n - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                ans[0] = mid;
-                high = mid - 1; 
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+        int low = 0;
+        int high = nums.size()-1;
+        vector <int> v;
+        if(nums.empty()){
+            v.push_back(-1);
+            v.push_back(-1);
+            return v;
         }
+       
+        
+int k = -1;
+while (low <= high) {
+    int mid = low + (high - low) / 2;
+    if(nums[mid]==target)
+   { k=mid;
+    high = mid - 1;}
 
-        low = 0; 
-        high = n - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                ans[1] = mid;
-                low = mid + 1; 
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
+    else if (nums[mid] > target) {
+       high = mid -1;
+    } else {
+      low = mid + 1;
     }
+}
+if(k!=-1)
+v.push_back(k);
+
+
+
+int lows = 0;
+ int highs = nums.size()-1;
+ int b = -1;
+
+
+while (lows <= highs) {
+    int mid = lows + (highs - lows) / 2;
+    if(nums[mid]==target)
+    {b = mid;
+    lows =  mid + 1;}
+
+    else if (nums[mid] < target) {
+        lows = mid + 1;
+    } else {
+        highs = mid - 1;
+    }
+}
+if(b!=-1)
+v.push_back(b);
+if(v.empty()){
+            v.push_back(-1);
+            v.push_back(-1);
+            return v;
+        }
+
+return v;
+
+
+
+
+        
+    } 
 };
