4fe1fb2cd43c6e90ac6bbf720c33a38d81ee5f68
atulpandey2626 <atulpandey262626@gmail.com>
2026-08-05 23:00:23 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.73%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int m = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(i>m)
+            return false;
+            int k = i + nums[i];
+            m = max(m,k);
+            
+        }
+        
+         return true;
+         
+       
+        
+    }
+};
