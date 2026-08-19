d2fcf370e1d30bc89423b71c7b15fd567248df2b
atulpandey2626 <atulpandey262626@gmail.com>
2026-08-19 16:22:38 +0530

    Time: 46 ms (37.44%), Space: 53.1 MB (11.5%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,19 +3,19 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX - 1));
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
 
         for (int i = 0; i <= n; i++) {
             dp[i][0] = 0;
+        }
+        for (int i = 1; i <= amount; i++) {
+            dp[0][i] = INT_MAX - 1;
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= amount; j++) {
                 if (coins[i - 1] <= j) {
-                    if (dp[i][j - coins[i - 1]] != INT_MAX - 1)
                         dp[i][j] = min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
-                    else
-                        dp[i][j] = dp[i - 1][j];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
