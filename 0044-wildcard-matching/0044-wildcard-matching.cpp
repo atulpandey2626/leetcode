38d330533de4708221f566c80c67ef572889aea6
atulpandey2626 <atulpandey262626@gmail.com>
2026-08-31 09:48:16 +0530

    Time: 60 ms (35.46%), Space: 15.9 MB (51.27%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    int dp[2001][2001];
-
-    bool solve(int i, int j, string &s, string &p) {
-
-        if(i == s.size() && j == p.size())
-            return true;
-
-        if(j == p.size())
-            return false;
-
-        if(i == s.size()) {
-            while(j < p.size()) {
-                if(p[j] != '*')
-                    return false;
-                j++;
-            }
-            return true;
-        }
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        if(s[i] == p[j] || p[j] == '?')
-            return dp[i][j] = solve(i+1, j+1, s, p);
-
-        if(p[j] == '*') {
-            return dp[i][j] = solve(i, j+1, s, p) ||
-                              solve(i+1, j, s, p);
-        }
-
-        return dp[i][j] = false;
-    }
-
     bool isMatch(string s, string p) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, s, p);
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+        // Both strings are empty
+        dp[0][0] = true;
+
+        // Empty s, so p must contain only '*'
+        for(int j = 1; j <= m; j++) {
+            if(p[j-1] == '*')
+                dp[0][j] = dp[0][j-1];
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+
+                if(p[j-1] == '?' || s[i-1] == p[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else if(p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
