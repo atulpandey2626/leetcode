eba79d50e2223b3b3b3946adcc12fe86fbd18bed
atulpandey2626 <atulpandey262626@gmail.com>
2026-08-31 09:10:18 +0530

    Time: 27 ms (70.87%), Space: 13.1 MB (79.66%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
+int dp[1001][1001];
+int solve(int i,int j,string &s, string &t){
+    if(j==t.size())
+    return 1;
+    if(i>=s.size()||j>t.size())
+    return 0;
+    if(dp[i][j] != -1)
+    return dp[i][j];
+    if(s[i]==t[j])
+    return dp[i][j] = solve(i+1,j+1,s,t) + solve(i+1,j,s,t);
+    else
+    return dp[i][j] = solve(i+1,j,s,t);
+}
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        vector<vector<unsigned long long>> dp (n+1,vector<unsigned long long>(m+1));
-        for(int i = 0;i<=n;i++){
-            dp[i][0] = 1;
-        }
-         for(int i = 1;i<=m;i++){
-            dp[0][i] = 0;
-        }
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(s[i-1]==t[j-1])
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                else
-                dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][m];
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,s,t);
         
     }
 };
